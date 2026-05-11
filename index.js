c7ad0f74d85275f53c20fc64cf0fdf385fa21610
Nguyen Van Vu <Vux@Nguyens-MacBook-Pro.local>
2026-05-12 02:20:05 +0700

    Session 7

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
-// RANDOM NUMBER GENERATOR
+// IF STATEMENT = If a condition is true, execute some code
+//                If not do something else
 
-// Math.random() generates a random number between 0 (inclusive) and 1 (exclusive). By adding 2, we shift the range to be between 2 (inclusive) and 3 (exclusive).
-
+const myText = document.getElementById("myText");
 const myButton = document.getElementById("myButton");
-const label1 = document.getElementById("label1");
-const label2 = document.getElementById("label2");
-const label3 = document.getElementById("label3");
-const min = 1;
-const max = 17;
+const myP = document.getElementById("myP");
+
 myButton.onclick = () => {
-  label1.textContent = Math.round(Math.random() * max - min);
-  label2.textContent = Math.round(Math.random() * max - min);
-  label3.textContent = Math.round(Math.random() * max - min);
+  let age = myText.value;
+  age = Number(age);
+
+  if (age > 100) {
+    myP.textContent = `You are too old to go this site!`;
+  } else if (age >= 18) {
+    myP.textContent = `You are old enough to go this site!`;
+  } else if (age == 0) {
+    myP.textContent = `You can't enter, you were just born`;
+  } else if (age < 0) {
+    myP.textContent = `Your age can't below 0!`;
+  } else {
+    myP.textContent = `You must be 18+ to go this site!`;
+  }
 };
