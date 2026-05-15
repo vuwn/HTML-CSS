8918a4e306753269ec1c3088a3fec2cd42f192fd
Nguyen Van Vu <Vux@Nguyens-MacBook-Pro.local>
2026-05-15 23:56:03 +0700

    Session 16

--- index.js
+++ index.js
@@ -1,18 +1,52 @@
-// DICE ROLLER PROGRAM
+// RANDOM PASSWORD GENERATOR
 
-function rollDice() {
-  const numOfDice = document.getElementById("numOfDice").value;
-  const diceResult = document.getElementById("diceResult");
-  const diceImages = document.getElementById("diceImages");
-  const values = [];
-  const images = [];
+function generatePassword(
+  length,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+) {
+  console.log("Generated password: ");
 
-  for (let i = 0; i < numOfDice; i++) {
-    const value = Math.floor(Math.random() * 6) + 1;
-    values.push(value);
-    images.push(`<img src="dice_images/${value}.png" alt ="Dice ${value}">`);
+  const lowercaseChars = "abcdefghijklmnopqrstuvwxyz";
+  const uppercaseChars = "ABCDEFGHIJKELMNOQRSTUVWXYZ";
+  const numberChars = "0123456789";
+  const symbolsChars = "!@#$%^&*()_+-=";
+
+  let allowedChars = "";
+  let password = "";
+
+  allowedChars += includeLowercase ? lowercaseChars : "";
+  allowedChars += includeUppercase ? uppercaseChars : "";
+  allowedChars += includeNumbers ? numberChars : "";
+  allowedChars += includeSymbols ? symbolsChars : "";
+
+  if (length < 0) {
+    return `Password length must be at least 1`;
+  }
+  if (allowedChars.length === 0) {
+    return `At least 1 set of character must be selected`;
   }
 
-  diceResult.textContent = `dice: ${values.join(",")}`;
-  diceImages.innerHTML = images.join("");
+  for (let i = 1; i < length; i++) {
+    const randomPassword = Math.floor(Math.random() * allowedChars.length);
+    password += allowedChars.charAt(randomPassword);
+  }
+  return password;
 }
+
+const numberLenght = 12;
+const includeLowercase = true;
+const includeUppercase = true;
+const includeNumbers = true;
+const includeSymbols = true;
+
+console.log(` ${generatePassword(
+  numberLenght,
+  includeLowercase,
+  includeUppercase,
+  includeNumbers,
+  includeSymbols
+)}
+  `);
