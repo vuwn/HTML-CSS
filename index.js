8d440edd52dfeb8e6c4787423815c3d044eed722
Nguyen Van Vu <Vux@Nguyens-MacBook-Pro.local>
2026-05-18 01:49:48 +0700

    Session 22

--- index.js
+++ index.js
@@ -1,18 +1,16 @@
-// sort()
 
+// Closure 
 
-const person = [{name: 'John', age: 30, gpa: 3.5}, 
-                {name: 'Karis', age: 25, gpa: 2.2}, 
-                {name: 'Mia', age: 28, gpa: 4.0},
-                {name: 'Alex', age: 22, gpa: 3.8}, 
-                {name: 'Emily', age: 27, gpa: 3.0}];
+function outer() {
+    let count = 0
 
-// Sort by name
-// person.sort((a, b) => a.name.localeCompare(b.name));
+  function inner() {
+    count++
+    console.log(count);
+  }
+  return inner;
+}
 
-// Sort by age
-person.sort((a, b) => a.age - b.age);
- 
-// sort by gpa
-person.sort((a, b) => a.gpa - b.gpa);
-console.log(person);
+const fn = outer();
+fn(); // 1
+fn(); // 2
