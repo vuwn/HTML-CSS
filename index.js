d1ac22082fb53242446eb0dae15de263b252e0e8
Nguyen Van Vu <Vux@Nguyens-MacBook-Pro.local>
2026-05-17 23:35:51 +0700

    Session 19

--- index.js
+++ index.js
@@ -1,54 +1,44 @@
-class Person {
-  constructor(firstName, lastName, age) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.age = age;
-  }
+// destructure = extract values from arrayd or objects
+// then assign them to variables in convienient way
+// [] = to perform array destructuring
+// {} = to perform object destructuring
 
-  set firstName(newFirstName) {
-    if (typeof newFirstName === "string" && newFirstName.length > 0) {
-      this._firstName = newFirstName;
-    } else {
-      console.error("First name must be a non_empty string");
-    }
-  }
+// SWAP 2 VARIABLES
+// let a = 1;
+// let b = 2;
+//  [a, b] = [b, a];
+//  console.log(a, b);
 
-  set lastName(newLastName) {
-    if (typeof newLastName === "string" && newLastName.length > 0) {
-      this._lastName = newLastName;
-    } else {
-      console.error("Last name must be a non_empty string");
-    }
-  }
 
-  set age(newAge) {
-    if (typeof newAge === "number" && newAge > 0) {
-      this._age = newAge;
-    } else {
-      console.error("Age must be a positive number");
-    }
-  }
+// SWAP 2 ELEMENT IN AN ARRAY
+//  const colors = ['blue', 'red', 'purple', 'yellow'];
+// [colors[0], colors[2] = colors[2], colors[0]];
+// console.log(colors);
 
-  get firstName() {
-    return this._firstName;
-  }
+// ASSIGN ARRAY ELEMENTS TO VARIABLES
+//  const colors = ['blue', 'red', 'purple', 'yellow', 'white'];
+// const [firstColor, secondColor, thirdColor, ...extraColor] =  colors;
+// console.log(firstColor, secondColor, thirdColor, extraColor);
 
-  get lastName() {
-    return this._lastName;
-  }
+// EXTRACT VALUES FROM OBJECTS
 
-  get fullName() {
-    return this._firstName + " " + this._lastName;
-  }
-
-  get age() {
-    return this._age;
-  }
+function displayPerson({firstName, lastName, age, job="Singer"}) {
+ console.log(`name: ${firstName} ${lastName}`);
+ console.log(`age: ${age}`);
+ console.log(`job: ${job}`);
 }
 
-const person = new Person("Vincent", "Nguyen", 18);
+const person1 = {
+  firstName: 'Vincent',
+  lastName: 'Nguyen',
+  age: 18,
+  job: 'Student'
+}
 
-console.log(person.firstName);
-console.log(person.lastName);
-console.log(person.fullName);
-console.log(person.age);
+const person2 = {
+  firstName: 'Talor',
+  lastName: 'Smith',
+  age: 30,
+}
+
+displayPerson(person1);
