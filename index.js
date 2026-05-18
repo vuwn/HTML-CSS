4993ff348fc4b4e3b0f0e8c4706e644a612ff676
Nguyen Van Vu <Vux@Nguyens-MacBook-Pro.local>
2026-05-19 00:53:19 +0700

    Session 24

--- index.js
+++ index.js
@@ -1,17 +1,53 @@
 
- 
- function updateClock() {
-  const now = new Date();
+const display = document.getElementById("display");
+console.log(display);
+let timer = null;
+let startTime = 0;
+let elapsedTime = 0;
+let isRunning = false;
 
-  const hours = now.getHours().toString().padStart(2, 0);
-  const meridiem = hours > 12 ? "PM" : "AM";
+function start(){
 
-  const minutes = now.getMinutes().toString().padStart(2, 0);
-  const seconds = now.getSeconds().toString().padStart(2, 0);
-  
-  const timeString = `${hours}:${minutes}:${seconds} ${meridiem}`;
-  document.getElementById('clock').textContent = timeString ;
- }
 
-updateClock();
-setInterval(updateClock, 1000);
+  if(!isRunning){
+    startTime = Date.now() - elapsedTime;
+    timer = setInterval(update, 10);
+    isRunning = true;
+  }
+}
+
+function stop(){
+
+  if(isRunning){
+    clearInterval(timer)
+    elapsedTime = Date.now() - startTime;
+    isRunning = false;
+  }
+}
+
+function reset(){
+
+  clearInterval(timer);
+  startTime = 0;
+  elapsedTime = 0;
+  isRunning = false;
+  display.textContent = "00:00:00:00";
+}
+
+function update(){
+
+  const currentTime = Date.now();
+  elapsedTime = currentTime - startTime;
+
+  let hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+  let minutes = Math.floor((elapsedTime / (1000 * 60) % 60));
+  let seconds = Math.floor((elapsedTime / 1000) % 60); 
+  let milliseconds = Math.floor((elapsedTime % 1000) / 10);
+
+  hours = hours.toString().padStart(2, '0');
+  minutes = minutes.toString().padStart(2, '0');  
+  seconds = seconds.toString().padStart(2, '0');
+  milliseconds = milliseconds.toString().padStart(2, '0');
+
+display.textContent = `${hours}:${minutes}:${seconds}:${milliseconds}`;
+}
