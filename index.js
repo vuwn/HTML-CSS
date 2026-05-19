228277cfd226ffc784ad5f548790d7385f984177
Nguyen Van Vu <Vux@Nguyens-MacBook-Pro.local>
2026-05-20 01:46:05 +0700

    Session 29

--- index.js
+++ index.js
@@ -1,32 +1,22 @@
-// element selectors
+// eventListener = Listens to specific events to create a interactive web pages
+//                 events: click, mouseover, mouseout, keydown,..
+// .addEventLister()
 
-// document.getElementById() - ELEMENT OR NULL
-// const heading = document.getElementById("heading");
-  // heading.style.backgroundColor = "yellow"
 
-// document.getElementsByClassName() - HTML COLLECTION
-const fruits = document.getElementsByClassName('fruits');
-Array.from(fruits).forEach(fruit => {
-  fruit.style.backgroundColor = "white"
-})
+const myBox = document.getElementById('myBox');
+const myButton = document.getElementById('myButton');
 
-// document.getElementsByTagName() - HTML COLLECTION
-// const h4Elements = document.getElementsByTagName('h4');
-// Array.from(h4Elements).forEach(h4Element => {
-//   h4Element.style.backgroundColor = "white"
-// })
-// const liElements = document.getElementsByTagName('li');
+myButton.addEventListener("click", event => {
+myBox.style.backgroundColor = 'tomato';
+myBox.textContent = 'Ouch';
+});
 
-// Array.from(liElements).forEach(liElement => {
-//   liElement.style.backgroundColor = "white"
-// })
+myButton.addEventListener("mouseover", event => {
+myBox.style.backgroundColor = 'yellow';
+myBox.textContent = 'Do not do it';
+});
 
-// document.querySelector() - FIRST ELEMENT OR NULL
-// const h4Elements = document.querySelector('li');
-// h4Elements.style.backgroundColor = "yellow"
-
-// document.querySelectorAll() - NODELIST
-const h4Elements = document.querySelectorAll('li');
-h4Elements.forEach(h4Element => {
-  h4Element.style.backgroundColor = "yellow"
-})
+myButton.addEventListener("mouseout", event => {
+myBox.style.backgroundColor = 'green';
+myBox.textContent = 'Click me';
+});
