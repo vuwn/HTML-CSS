cf1b81a232a0bc03fea4c8ce184e0ce04ec6f726
Nguyen Van Vu <Vux@Nguyens-MacBook-Pro.local>
2026-05-20 00:45:36 +0700

    Session 28

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
-// CALCULATOR PROGRAMS
+// element selectors
 
-const display = document.getElementById('display');
+// document.getElementById() - ELEMENT OR NULL
+// const heading = document.getElementById("heading");
+  // heading.style.backgroundColor = "yellow"
 
-function appendToDisplay(input) {
-    display.value += input;
-}
+// document.getElementsByClassName() - HTML COLLECTION
+const fruits = document.getElementsByClassName('fruits');
+Array.from(fruits).forEach(fruit => {
+  fruit.style.backgroundColor = "white"
+})
 
-function clearDisplay() {
-    display.value = '';
-}
+// document.getElementsByTagName() - HTML COLLECTION
+// const h4Elements = document.getElementsByTagName('h4');
+// Array.from(h4Elements).forEach(h4Element => {
+//   h4Element.style.backgroundColor = "white"
+// })
+// const liElements = document.getElementsByTagName('li');
 
-function calculate() {
-  if(display.value === '1+1') {
-    display.value = 3;
-  } else {  
-      try{
-        display.value = eval(display.value);
-      } catch(error) {
-         display.value = 'Error';
-     }
-    }
-}
+// Array.from(liElements).forEach(liElement => {
+//   liElement.style.backgroundColor = "white"
+// })
+
+// document.querySelector() - FIRST ELEMENT OR NULL
+// const h4Elements = document.querySelector('li');
+// h4Elements.style.backgroundColor = "yellow"
+
+// document.querySelectorAll() - NODELIST
+const h4Elements = document.querySelectorAll('li');
+h4Elements.forEach(h4Element => {
+  h4Element.style.backgroundColor = "yellow"
+})
