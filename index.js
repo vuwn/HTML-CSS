5df86f86d13f9c515d39fd15157e75e30c2affd6
Nguyen Van Vu <Vux@Nguyens-MacBook-Pro.local>
2026-05-20 22:03:48 +0700

    Session 32

--- index.js
+++ index.js
@@ -1,16 +1,44 @@
-// addEventListener: hide, show the HTML
 
-const myButton = document.getElementById("myButton");
-const myImage = document.getElementById('myImage');
+// Nodelist 
 
-myButton.addEventListener('click', event => {
+let buttons = document.querySelectorAll('.myButton');
+console.log(buttons);
 
-    if(myImage.style.visibility === `hidden`){
-        myImage.style.visibility = `visible`;
-        myButton.textContent = `Hide`;
-    } else {
-        myImage.style.visibility = `hidden`;
-        myButton.textContent = `Show`;
-    }
 
-});
+
+// buttons.forEach(button => {
+//     button.addEventListener("click", event => {
+//         event.target.style.backgroundColor = "tomato"
+//     })
+// })
+
+    buttons.forEach(button => {
+        button.addEventListener('mouseover', event => {
+            event.target.style.backgroundColor = 'hsl(193, 73%, 40%)'
+        })
+    })
+
+    buttons.forEach(button => {
+        button.addEventListener('mouseout', event => {
+            event.target.style.backgroundColor = 'hsl(193, 73%, 50%)'
+        })
+    })
+
+// ADD A ELEMENT;
+
+    let newButton = document.createElement('button');
+    newButton.textContent = "Button 5";
+    newButton.classList = "myButton";
+    document.body.appendChild(newButton);
+    buttons = document.querySelectorAll('.myButton');
+    console.log(buttons);
+    
+// REMOVE A ELEMENT
+// Node list will not update automatically reflect changes
+    buttons.forEach(button => {
+        button.addEventListener("click", event => {
+            event.target.remove();
+            buttons = document.querySelectorAll('.myButton');
+            console.log(buttons);
+        })
+    })
